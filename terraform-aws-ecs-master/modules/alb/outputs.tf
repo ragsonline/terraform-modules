8c70f1857ff70e7d87535ff4e412cc622f6d6088
Raghavendra.Manjunath <raghavendra.manjunath01@service.nsw.gov.au>
2021-10-26 13:20:09 +1100

    fix acm

--- terraform-aws-ecs-master/modules/alb/outputs.tf
+++ terraform-aws-ecs-master/modules/alb/outputs.tf
@@ -1,7 +1,7 @@
-output "listener_https_arn" {
-  description = "The ARN of the HTTPS ALB Listener that can be used to add rules"
-  value       = module.alb.https_listener_arns[0]
-}
+#output "listener_https_arn" {
+#  description = "The ARN of the HTTPS ALB Listener that can be used to add rules"
+#  value       = module.alb.https_listener_arns[0]
+#}
 
 output "target_group_arn" {
   description = "ARN of the target group"
